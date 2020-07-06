f3442ae9d89aac4a9f88c84c2389c66ec6981759
Илья Подгорнов <lokkibro@outlook.com>
2020-07-06 18:26:24 +0300

    Написал Двухсвязный список

---
+++ LinkedList/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node<T>
+    {
+        
+        public Node<T> Next { get; internal set; }
+
+        public Node<T> Previous { get; internal set; }
+
+        public T Value { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }                    
+
+    }
+}
